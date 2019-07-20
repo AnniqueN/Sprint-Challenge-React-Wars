7cd0a02807999c3d61c44b5b5d6fcf50192c5512
Annique Nsabimana <anniquensabimana@gmail.com>
2019-07-19 22:23:32 -0500

    createdCardStyle

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
+import { Card} from 'semantic-ui-react';
 
 export default class Cards extends Component {
   render() {
     console.log(this.props.data);
     return (
-      <div className="card center">
+      <Card className="card center">
         <img
           className="card-img-top"
           src="https://cdn.pixabay.com/photo/2016/01/15/12/41/disney-1141607_1280.jpg"
           alt="Star Wars"
         />
-        <div className="card-body">
+        <card className="card-body">
           <h1 className="card-title">Name: {this.props.data.name}</h1>
-          <div className="card-text">
+          <card className="card-text">
             <h5 className="mb-0 mt-3">
               <span className="mr-1 ml-2 font-weight-bold">BirthYear :</span>
               <span className="ml-1">{this.props.data.birth_year}</span>
             </h5>
-          </div>
-        </div>
-      </div>
+          </card>
+        </card>
+      </Card>
     );
   }
 }
